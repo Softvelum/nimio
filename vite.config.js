16f928399eb21ff6f982802ce7cd44c5ee6b88f3
Dmitry Shovtyuk <shovtyuk@gmail.com>
2025-05-13 22:09:02 -0400

    lib mode demo page

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,37 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/nimio.js"),
+      formats: ["es"],
     },
-    plugins: [
-        {
-            // COOP/COEP required for SharedArrayBuffer
-            name: 'vite-plugin-coop-coep',
-            // `npm run dev`
-            configureServer(server) {
-                server.middlewares.use((req, res, next) => {
-                    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
-                    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
-                    next()
-                })
-            },
-            // `npm run preview`
-            configurePreviewServer(server) {
-                server.middlewares.use((req, res, next) => {
-                    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
-                    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
-                    next()
-                })
-            },
-        },
-    ],
-})
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+  },
+  plugins: [
+    {
+      // COOP/COEP required for SharedArrayBuffer
+      name: "vite-plugin-coop-coep",
+      // `npm run dev`
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+          next();
+        });
+      },
+      // `npm run preview`
+      configurePreviewServer(server) {
+        server.middlewares.use((req, res, next) => {
+          res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+          next();
+        });
+      },
+    },
+  ],
+});
