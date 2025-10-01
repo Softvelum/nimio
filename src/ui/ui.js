beadadb623312de64429ea3b356d04a24c575d09
Dmitry Shovtyuk <shovtyuk@gmail.com>
2025-10-01 01:12:45 -0400

    UI (#23)
    * UI initial implementation
    
    * added play-pause behaviour
    
    * added volume controls
    
    * prettier

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -1,9 +1,12 @@
 import {} from "./ui.css";
 import { DebugView } from "./debug-view";
+import controlsHtml from "./controls.html?raw";
+import controlsCss from "./controls.css?raw";
 
 export class Ui {
-  constructor(container, opts, onPlayPause) {
+  constructor(container, opts, onPlayPause, onMuteUnmute, onVolumeChange) {
     this.state = "pause";
+    this.muted = false;
 
     this.container = document.getElementById(container);
     Object.assign(this.container.style, {
@@ -30,9 +33,39 @@ export class Ui {
     this.container.appendChild(this.btnPlayPause);
 
     this._onClick = (e) => this._hanldleClick(e, onPlayPause);
-    this.container.addEventListener("click", this._onClick);
+    this._onMuteUnmuteClick = () => this._hanldleMuteUnmuteClick(onMuteUnmute);
+    this._onVolumeChange = (value) =>
+      this._hanldleVolumeChange(value, onVolumeChange);
+    this.canvas.addEventListener("click", this._onClick);
+
+    this._createControls();
 
     this.setupEasing();
+  }
+
+  _createControls() {
+    const tpl = document.createElement("template");
+    tpl.innerHTML = controlsHtml.trim();
+
+    const frag = tpl.content.cloneNode(true);
+    this.controlsBar = frag.querySelector(".nimio-controls");
+
+    this.container.appendChild(frag);
+
+    this.buttonPlayPause = this.controlsBar.querySelector(".btn-play-pause");
+    this.buttonPlayPause.addEventListener("click", this._onClick);
+
+    this.buttonVolume = this.controlsBar.querySelector(".btn-volume");
+    this.buttonVolume.addEventListener("click", this._onMuteUnmuteClick);
+
+    this.volumeRange = this.controlsBar.querySelector(".volume-range");
+    this.volumeRange.addEventListener("input", (e) => {
+      this._onVolumeChange(Number(e.target.value));
+    });
+
+    const style = document.createElement("style");
+    style.textContent = controlsCss;
+    document.head.appendChild(style);
   }
 
   destroy() {
@@ -67,22 +100,32 @@ export class Ui {
     this.state = "pause";
     this.button.classList.remove("pause");
     this.button.classList.add("play");
+    this.buttonPlayPause.querySelector(".icon-play").style.display = "block";
+    this.buttonPlayPause.querySelector(".icon-pause").style.display = "none";
   }
 
   drawPause() {
     this.state = "play";
     this.button.classList.remove("play");
     this.button.classList.add("pause");
+    this.buttonPlayPause.querySelector(".icon-play").style.display = "none";
+    this.buttonPlayPause.querySelector(".icon-pause").style.display = "block";
   }
 
   showControls(animate) {
     this.btnPlayPause.style.transition = animate ? "opacity 0.2s ease" : "none";
     this.btnPlayPause.style.opacity = "0.7";
+
+    this.controlsBar.style.transition = animate ? "opacity 0.2s ease" : "none";
+    this.controlsBar.style.opacity = "1";
   }
 
   hideControls(animate) {
     this.btnPlayPause.style.transition = animate ? "opacity 0.5s ease" : "none";
     this.btnPlayPause.style.opacity = "0";
+
+    this.controlsBar.style.transition = animate ? "opacity 0.2s ease" : "none";
+    this.controlsBar.style.opacity = "0";
   }
 
   get size() {
@@ -98,7 +141,30 @@ export class Ui {
     } else {
       this.drawPlay();
     }
-    onPlayPause(e, isPlayClicked);
+    onPlayPause?.(e, isPlayClicked);
+  }
+
+  _hanldleMuteUnmuteClick(onMuteUnmute) {
+    this.muted = !this.muted;
+    if (this.muted) {
+      this.buttonVolume.querySelector(".icon-vol-mute").style.display = "none";
+      this.buttonVolume.querySelector(".icon-vol-unmute").style.display =
+        "block";
+    } else {
+      this.buttonVolume.querySelector(".icon-vol-mute").style.display = "block";
+      this.buttonVolume.querySelector(".icon-vol-unmute").style.display =
+        "none";
+    }
+    onMuteUnmute?.(this.muted);
+  }
+
+  _hanldleVolumeChange(value, onVolumeChange) {
+    if (0 == value && !this.muted) {
+      this._onMuteUnmuteClick();
+    } else if (this.muted) {
+      this._onMuteUnmuteClick();
+    }
+    onVolumeChange?.(value);
   }
 
   _handleMouseMove(e) {
