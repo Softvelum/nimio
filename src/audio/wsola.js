37eadbebeb5e785464c34020734ff5d2151624c3
agonchar <goncharov@wmspanel.com>
2025-11-28 00:31:56 +1000

    Start implementing WSOLA algorithm

---
+++ src/audio/wsola.js
@@ -0,0 +1,38 @@
+const WEBAUDIO_BLOCK_SIZE = 128;
+
+function makeHannWindow(N) {
+  let win = new Float32Array(N);
+  for (var n = 0; n < N; n++) {
+    win[n] = 0.5 * (1 - Math.cos(2 * Math.PI * n / N));
+  }
+
+  return win;
+}
+
+function applyHannWindow(input, N, window) {
+  for (var n = 0; n < N; n++) {
+    input[n] *= window[n];
+  }
+}
+
+function dotProduct(a, aOff, b, bOff, length) {
+  let res = 0;
+  for (let i = 0; i < length; i++) {
+    res += a[aOff + i] * b[bOff + i];
+  }
+  return res;
+}
+
+function l2EnergyArraySegment(arr, offset, length) {
+  let res = 0;
+  for (let i = 0; i < length; i++) {
+    const v = arr[offset + i];
+    res += v * v;
+  }
+
+  return res;
+}
+
+
+
+
